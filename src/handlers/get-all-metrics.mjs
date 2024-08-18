813664eeeeef78e0507a223f0e3f95ed8f715fb6
satishnvrn <nvrn.satish@gmail.com>
2024-08-18 17:50:24 +0530

    initial commit

---
+++ src/handlers/get-all-metrics.mjs
@@ -0,0 +1,32 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.PRODUCT_METRICS_TABLE;
+
+export const getAllMetricsHandler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getAllMetrics only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    console.info('received:', event);
+
+    var params = {
+        TableName : tableName
+    };
+
+    try {
+        const data = await ddbDocClient.send(new ScanCommand(params));
+        var items = data.Items;
+    } catch (err) {
+        console.log("Error", err);
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(items)
+    };
+
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
